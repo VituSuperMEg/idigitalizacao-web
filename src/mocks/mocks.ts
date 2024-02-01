827b05bdb07e6f60e2b815be59f5af51422a11e6
VituSuperMEg <vitorpires8555@gmail.com>
2024-02-01 07:31:55 -0300

    init crud ✌️🐬

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,4 @@
+export const orgaos = [
+  { codigo : '1', nome : 'SECRETARIA MUNICIPAL DE GOVERNO', responsavel : 'Antônio Lisboa de Souza', cpf : '000000000000'},
+  { codigo : '2', nome : 'SECRETARIA MUNICIPAL DE ADMINISTRAÇÃO E FINANÇAS', responsavel : 'Marta Celia Batista Vieira', cpf : '000000000020'}
+]
