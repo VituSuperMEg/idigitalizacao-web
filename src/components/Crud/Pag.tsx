4de424fdefa8b126e0e02090b5735630073dcded
VituSuperMEg <vitorpires8555@gmail.com>
2024-02-20 13:47:15 -0300

    🗃️🗃️Nova versão da paginação - 0.0.2

--- src/components/Crud/Pag.tsx
+++ src/components/Crud/Pag.tsx
@@ -1,30 +1,49 @@
 import { useCrud } from '@/store/crud';
 import { If } from 'if-component-ts';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 export default function Pagination() {
+  // react
+  const [total, setTotal] = useState(10);
 
+  // zustand
   const currentPage = useCrud(state => state.curretPage);
   const setCurrentPage = useCrud(state => state.setCurretPage);
+  const totalPages = useCrud(state => state.totalPages);
+  const lastPage = useCrud(state => state.lastPage);
 
   const handlePageChange = (page: any) => {
     setCurrentPage(page);
   };
 
+  function caclTotal() {
+    setTotal(totalPages - total);
+  }
+  useEffect(() => {
+    currentPage !== 1 && caclTotal()
+
+  }, [currentPage]);
+
   return (
-    <div className="flex items-center gap-3 mt-2" style={{ float: 'left'}}>
+    <div className="flex items-center gap-3 mt-2" style={{ float: 'left' }}>
       <If test={currentPage > 1}>
-      <button onClick={() => handlePageChange(currentPage - 1)} className="border p-2" style={{ borderRadius :  "20%"}}>
-        Anterior
-      </button>
+        <button onClick={() => handlePageChange(currentPage - 1)} className="border p-2" style={{ borderRadius: "20%" }}>
+          Anterior
+        </button>
       </If>
-      <div className="bg-slate-500 flex items-center justify-center" style={{ width: 30, height: 30, borderRadius : '50%'}}>
+      mostrando {total} de {totalPages}
+      <div className="bg-slate-500 flex items-center justify-center" style={{ width: 30, height: 30, borderRadius: '50%' }}>
         <span className="text-white">
-        {currentPage}
+          1
         </span>
       </div>
-      <button onClick={() => handlePageChange(currentPage + 1)} className="border p-2" style={{ borderRadius :  "20%"}}>Próxima</button>
+      <div className="bg-slate-500 flex items-center justify-center" style={{ width: 30, height: 30, borderRadius: '50%' }}>
+        <span className="text-white">
+          {lastPage}
+        </span>
+      </div>
+      <button onClick={() => handlePageChange(currentPage + 1)} className="border p-2" style={{ borderRadius: "20%" }}>Próxima</button>
     </div>
   );
 }
