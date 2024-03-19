704817ab3ef045c91909e3e2214dacfc370c1a6e
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-19 14:32:31 -0300

    Main info com o dropdown

--- src/components/Panel/components/MainInfo.tsx
+++ src/components/Panel/components/MainInfo.tsx
@@ -13,24 +13,24 @@ export default function MainInfo() {
 
   return (
     <div className="avatar">
-      <DropdownMenu.Root >
+      <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
           <div className="flex items-center gap-2 mt-5 cursor-pointer">
             <Avatar name={user.name} image={user.image} />
           </div>
         </DropdownMenu.Trigger>
-        <DropdownMenu.Content className="bg-white mr-6 mt-4 w-44 h-36 rounded-md">
-          <DropdownMenu.Item  className="flex items-center gap-5 border-0">
-            <User /> Meu perfil
+        <DropdownMenu.Content className="bg-white mr-4 p-3 flex flex-col gap-2 mt-4 h-36 rounded-md w-80 shadow-sm">
+          <DropdownMenu.Item className="flex items-center gap-5 border-0 outline-none">
+            <User className="text-zinc-500" /> Meu perfil
           </DropdownMenu.Item>
-          <DropdownMenu.Item  className="flex items-center gap-5 border-0">
-            <Settings /> Configurações
+          <DropdownMenu.Item  className="flex items-center gap-5 border-0 outline-none">
+            <Settings className="text-zinc-500" /> Configurações
           </DropdownMenu.Item>
-          <DropdownMenu.Item  className="flex items-center gap-5 border-0">
-            <MessageSquare /> Notificações
+          <DropdownMenu.Item  className="flex items-center gap-5 border-0 outline-none">
+            <MessageSquare className="text-zinc-500" /> Notificações
           </DropdownMenu.Item>
-          <DropdownMenu.Item  className="flex items-center gap-5 border-0">
-            <LogOut /> Sair
+          <DropdownMenu.Item  className="flex items-center gap-5 border-0 outline-none text-red-500">
+            <LogOut className="text-red-500" /> Sair
           </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
